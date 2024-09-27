1bf949d803fd5f0dc3ae654ad17b8fdaa79ce4a8
shanME <giorgie103188@gmail.com>
2024-09-27 16:50:35 +0400

    components file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
